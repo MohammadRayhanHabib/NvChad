ba5449b9db191feceb59941e7e018498a9d2574e
MohammadRayhanHabib <your_username@users.noreply.github.com>
2025-03-05 09:25:50 +0600

    nvim-config

---
+++ lua/configs/lspconfig.lua
@@ -0,0 +1,94 @@
+local on_attach =
+    require("nvchad.configs.lspconfig").on_attach
+local on_init =
+    require("nvchad.configs.lspconfig").on_init
+local capabilities = require(
+    "nvchad.configs.lspconfig"
+).capabilities
+
+local lspconfig = require("lspconfig")
+
+-- list of all servers configured.
+lspconfig.servers = {
+    "lua_ls",
+    "clangd",
+    "pyright",
+    "ts_ls", -- JavaScript/TypeScript
+    "html", -- HTML
+    "cssls", -- CSS
+    "emmet_ls", -- Emmet (for fast HTML/CSS)
+    "tailwindcss", -- Tailwind CSS
+    "eslint", -- ESLint
+    "jsonls", -- JSON
+    "prismals", -- Prisma
+    "graphql", -- GraphQL
+    "dockerls", -- Docker
+    "yamlls", -- YAML
+}
+
+-- list of servers configured with default config.
+local default_servers = {
+    "pyright",
+    "ts_ls",
+    "html",
+    "cssls",
+    "emmet_ls",
+    "tailwindcss",
+    "eslint",
+    "jsonls",
+    "prismals",
+    "graphql",
+    "dockerls",
+    "yamlls",
+}
+
+-- lsps with default config
+for _, lsp in ipairs(default_servers) do
+    lspconfig[lsp].setup({
+        on_attach = on_attach,
+        on_init = on_init,
+        capabilities = capabilities,
+    })
+end
+
+lspconfig.clangd.setup({
+    on_attach = function(client, bufnr)
+        client.server_capabilities.documentFormattingProvider =
+            false
+        client.server_capabilities.documentRangeFormattingProvider =
+            false
+        on_attach(client, bufnr)
+    end,
+    on_init = on_init,
+    capabilities = capabilities,
+})
+
+lspconfig.lua_ls.setup({
+    on_attach = on_attach,
+    on_init = on_init,
+    capabilities = capabilities,
+    settings = {
+        Lua = {
+            diagnostics = {
+                enable = false, -- Disable all diagnostics from lua_ls
+            },
+            workspace = {
+                library = {
+                    vim.fn.expand(
+                        "$VIMRUNTIME/lua"
+                    ),
+                    vim.fn.expand(
+                        "$VIMRUNTIME/lua/vim/lsp"
+                    ),
+                    vim.fn.stdpath("data")
+                        .. "/lazy/ui/nvchad_types",
+                    vim.fn.stdpath("data")
+                        .. "/lazy/lazy.nvim/lua/lazy",
+                    "${3rd}/love2d/library",
+                },
+                maxPreload = 100000,
+                preloadFileSize = 10000,
+            },
+        },
+    },
+})
---
+++ lua/plugins/init.lua
@@ -0,0 +1,157 @@
+-- return {
+--
+--     {
+--         "nvim-treesitter/nvim-treesitter",
+--         event = { "BufReadPre", "BufNewFile" },
+--         config = function()
+--             require("configs.treesitter")
+--         end,
+--     },
+--
+--     {
+--         "neovim/nvim-lspconfig",
+--         event = { "BufReadPre", "BufNewFile" },
+--         config = function()
+--             require("nvchad.configs.lspconfig").defaults()
+--             require("configs.lspconfig")
+--         end,
+--     },
+--
+--     {
+--         "williamboman/mason-lspconfig.nvim",
+--         event = "VeryLazy",
+--         dependencies = { "nvim-lspconfig" },
+--         config = function()
+--             require("configs.mason-lspconfig")
+--         end,
+--     },
+--
+--     {
+--         "mfussenegger/nvim-lint",
+--         event = { "BufReadPre", "BufNewFile" },
+--         config = function()
+--             require("configs.lint")
+--         end,
+--     },
+--
+--     {
+--         "rshkarin/mason-nvim-lint",
+--         event = "VeryLazy",
+--         dependencies = { "nvim-lint" },
+--         config = function()
+--             require("configs.mason-lint")
+--         end,
+--     },
+--
+--     {
+--         "stevearc/conform.nvim",
+--         event = "BufWritePre",
+--         config = function()
+--             require("configs.conform")
+--         end,
+--     },
+--
+--     {
+--         "zapling/mason-conform.nvim",
+--         event = "VeryLazy",
+--         dependencies = { "conform.nvim" },
+--         config = function()
+--             require("configs.mason-conform")
+--         end,
+--     },
+-- }
+
+return {
+    -- Treesitter for better syntax highlighting
+    {
+        "nvim-treesitter/nvim-treesitter",
+        event = { "BufReadPre", "BufNewFile" },
+        config = function()
+            require("configs.treesitter")
+        end,
+    },
+
+    -- LSP Configuration
+    {
+        "neovim/nvim-lspconfig",
+        event = { "BufReadPre", "BufNewFile" },
+        config = function()
+            require("nvchad.configs.lspconfig").defaults()
+            require("configs.lspconfig")
+        end,
+    },
+
+    -- Mason LSP Config
+    {
+        "williamboman/mason-lspconfig.nvim",
+        event = "VeryLazy",
+        dependencies = { "nvim-lspconfig" },
+        config = function()
+            require("configs.mason-lspconfig")
+        end,
+    },
+
+    -- Linter
+    {
+        "mfussenegger/nvim-lint",
+        event = { "BufReadPre", "BufNewFile" },
+        config = function()
+            require("configs.lint")
+        end,
+    },
+
+    -- Mason Linter
+    {
+        "rshkarin/mason-nvim-lint",
+        event = "VeryLazy",
+        dependencies = { "nvim-lint" },
+        config = function()
+            require("configs.mason-lint")
+        end,
+    },
+
+    -- Formatter
+    {
+        "stevearc/conform.nvim",
+        event = "BufWritePre",
+        config = function()
+            require("configs.conform")
+        end,
+    },
+
+    -- Mason Formatter
+    {
+        "zapling/mason-conform.nvim",
+        event = "VeryLazy",
+        dependencies = { "conform.nvim" },
+        config = function()
+            require("configs.mason-conform")
+        end,
+    },
+
+    -- 🌈 Tailwind Color Preview (nvim-colorizer.lua)
+    {
+        "NvChad/nvim-colorizer.lua",
+        event = "VeryLazy",
+        config = function()
+            require("colorizer").setup({
+                user_default_options = {
+                    names = false, -- Don't highlight color names (e.g., "red")
+                    css = true, -- Enable CSS color highlighting
+                    tailwind = true, -- Enable Tailwind color previews
+                },
+            })
+        end,
+    },
+
+    -- 🎨 Tailwind Autocomplete Color Preview (Optional)
+    -- {
+    --     "roobert/tailwindcss-colorizer-cmp.nvim",
+    --     event = "VeryLazy",
+    --     config = function()
+    --         require("tailwindcss-colorizer-cmp").setup({
+    --             color_square_width = 2, -- Adjust the size of the color preview in autocomplete
+    --         })
+    --     end,
+    -- },
+}
